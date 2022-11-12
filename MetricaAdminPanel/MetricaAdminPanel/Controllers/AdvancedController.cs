7439396ba4ff6a076cfa0d3c23601c5163b6e150
AslanHasanov <aslanrh@code.edu.az>
2022-11-12 23:45:52 +0400

    AdvancedController folder created and initialized

---
+++ MetricaAdminPanel/MetricaAdminPanel/Controllers/AdvancedController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MetricaAdminPanel.Controllers
+{
+    public class AdvancedController : Controller
+    {
+        public ActionResult Animation()
+        {
+            return View();
+        }
+    }
+}
