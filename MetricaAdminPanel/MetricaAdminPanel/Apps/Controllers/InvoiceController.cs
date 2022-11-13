f54e219a06f40820f31ade6edb7b8a3273c323a0
AslanHasanov <aslanrh@code.edu.az>
2022-11-13 18:17:59 +0400

    Invoice module and Apps section completed

---
+++ MetricaAdminPanel/MetricaAdminPanel/Apps/Controllers/InvoiceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MetricaAdminPanel.Apps.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
