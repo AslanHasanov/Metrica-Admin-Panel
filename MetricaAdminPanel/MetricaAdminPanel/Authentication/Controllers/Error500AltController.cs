fe60e79f91726fbac3506206c63fde49650e0d2e
AslanHasanov <aslanrh@code.edu.az>
2022-11-13 19:38:34 +0400

    Error500Alt module created and completed.

---
+++ MetricaAdminPanel/MetricaAdminPanel/Authentication/Controllers/Error500AltController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MetricaAdminPanel.Authentication.Controllers
+{
+    public class Error500AltController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
