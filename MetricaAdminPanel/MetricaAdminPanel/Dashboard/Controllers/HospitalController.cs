a9dc120958e62ea8e24289b30b23a79bff021a1a
AslanHasanov <aslanrh@code.edu.az>
2022-11-13 17:41:02 +0400

    Hospital module created and completed, Dashboard section completed

---
+++ MetricaAdminPanel/MetricaAdminPanel/Dashboard/Controllers/HospitalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MetricaAdminPanel.Dashboard.Controllers
+{
+    public class HospitalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
