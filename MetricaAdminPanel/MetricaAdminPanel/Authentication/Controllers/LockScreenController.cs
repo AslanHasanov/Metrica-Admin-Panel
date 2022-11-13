fcaa2a432fa8e5450c43c27bdf743c2426b2ac6f
AslanHasanov <aslanrh@code.edu.az>
2022-11-13 19:27:42 +0400

    LockScreen module created and completed

---
+++ MetricaAdminPanel/MetricaAdminPanel/Authentication/Controllers/LockScreenController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MetricaAdminPanel.Authentication.Controllers
+{
+    public class LockScreenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
