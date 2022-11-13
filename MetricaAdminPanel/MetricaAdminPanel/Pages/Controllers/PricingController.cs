e8412551686ce9988cd014d2a3dfd6149d4f28d0
AslanHasanov <aslanrh@code.edu.az>
2022-11-13 18:39:15 +0400

    Pricing module created and completed

---
+++ MetricaAdminPanel/MetricaAdminPanel/Pages/Controllers/PricingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MetricaAdminPanel.Pages.Controllers
+{
+    public class PricingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
