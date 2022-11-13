3a987f7f33f92726203f576ebb321da5b409aff1
AslanHasanov <aslanrh@code.edu.az>
2022-11-13 18:30:20 +0400

    TimeLine module created and completed

---
+++ MetricaAdminPanel/MetricaAdminPanel/Pages/Controllers/TimeLineController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MetricaAdminPanel.Pages.Controllers
+{
+    public class TimeLineController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
