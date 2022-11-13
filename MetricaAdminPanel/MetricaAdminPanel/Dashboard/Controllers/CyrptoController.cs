b0975cdeebf04326f4ec337a9d2311eb91d5ab32
AslanHasanov <aslanrh@code.edu.az>
2022-11-13 16:05:44 +0400

    Cyrpto module Created and completed

---
+++ MetricaAdminPanel/MetricaAdminPanel/Dashboard/Controllers/CyrptoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MetricaAdminPanel.Dashboard.Controllers
+{
+    public class CyrptoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
