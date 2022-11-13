0fb3a07af0cce28acd9e3f497b1ba4b1f5a815bb
AslanHasanov <aslanrh@code.edu.az>
2022-11-13 17:36:18 +0400

    Helpdesk module created and completed

---
+++ MetricaAdminPanel/MetricaAdminPanel/Dashboard/Controllers/HelpdeskController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MetricaAdminPanel.Dashboard.Controllers
+{
+    public class HelpdeskController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
