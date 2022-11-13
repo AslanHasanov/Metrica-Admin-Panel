b655ee133e6969a0b4bec9ffb50354bd200ab5bd
AslanHasanov <aslanrh@code.edu.az>
2022-11-13 18:33:33 +0400

    Treeview module created and completed

---
+++ MetricaAdminPanel/MetricaAdminPanel/Pages/Controllers/TreeviewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MetricaAdminPanel.Pages.Controllers
+{
+    public class TreeviewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
