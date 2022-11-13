fe83ade297e5fce7da3de70081ce450a5737d5cc
AslanHasanov <aslanrh@code.edu.az>
2022-11-13 18:36:48 +0400

    Starter page created and completed

---
+++ MetricaAdminPanel/MetricaAdminPanel/Pages/Controllers/StarterPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MetricaAdminPanel.Pages.Controllers
+{
+    public class StarterPageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
