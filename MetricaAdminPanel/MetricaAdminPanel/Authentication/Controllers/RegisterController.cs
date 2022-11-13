890d84f34099465c0c69991eae00493eefe8b119
AslanHasanov <aslanrh@code.edu.az>
2022-11-13 19:14:39 +0400

    Register module created and completed

---
+++ MetricaAdminPanel/MetricaAdminPanel/Authentication/Controllers/RegisterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MetricaAdminPanel.Authentication.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
